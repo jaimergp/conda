97d00e415e9c90f82e89096dea33455b45f17454
jaimergp <jaimergp@users.noreply.github.com>
2026-01-23 12:58:07 +0100

    revert this change

--- tests/core/test_solve.py
+++ tests/core/test_solve.py
@@ -3195,13 +3195,17 @@ def test_freeze_deps_1(tmpdir):
             (
                 "channel-2::six-1.7.3-py34_0",
                 "channel-2::python-3.4.5-0",
-                # LIBMAMBA ADJUSTMENT
-                # libmamba v1 don't remove xz in this solve
+                # LIBMAMBA/RATTLER ADJUSTMENT
+                # libmamba v1 / rattler don't remove xz in this solve
                 *(
                     ()
                     if (
-                        context.solver == "libmamba"
-                        and VersionOrder(version("libmambapy")) < VersionOrder("2.0a0")
+                        context.solver == "rattler"
+                        or (
+                            context.solver == "libmamba"
+                            and VersionOrder(version("libmambapy"))
+                            < VersionOrder("2.0a0")
+                        )
                     )
                     else ("channel-2::xz-5.2.3-0",)
                 ),
